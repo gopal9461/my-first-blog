78f216a42669fa70b050a223955c94c9f53be0bc
gopal9461 <goopaalkhadka@gmail.com>
2020-06-03 12:13:31 +0545

    change done

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'djangogirls.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'djangoGirls',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         # 'USER': 'root',
         # 'PASSWORD': '8126',
         # 'HOST': 'localhost',
